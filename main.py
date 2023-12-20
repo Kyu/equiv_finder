3e3c64101a843102883eaa9f1a6d050c05a865cb
Precious Oyetunji <Kyu@users.noreply.github.com>
2023-12-20 10:40:16 -0600

    Add args

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import sys
+
 from selenium import webdriver
 from selenium.common.exceptions import NoSuchElementException
 
@@ -18,26 +20,37 @@ do = Action(driver, ("CS 101",))
 # do.select_all_subjects()  # selenium.common.exceptions.InvalidSelectorException
 
 state_id = 0
+
+argc = len(sys.argv)
+
+if argc == 2:
+    state_id = int(sys.argv[1])
+else:
+    state_id = 0
+
+if argc == 3:
+    school_id = int(sys.argv[2])
+else:
+    school_id = 0
+
 while True:
     state_id += 1
     print(f"State ID = {state_id}")
     try:
         do.select_state(state_id)
-        school_id = 0
         while True:
             school_id += 1
             try:
                 do.select_school(school_id)
                 do.select_all_subjects()
                 do.submit_form()
-                # check page for xyz
+
                 do.find_text_in_table()
                 do.go_back()
             except NoSuchElementException:
                 break
     except NoSuchElementException:
         break
-
 
 input("Press enter to exit")
 driver.close()
